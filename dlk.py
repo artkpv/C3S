f28a400cfa6bc9ffdeda35ab467a0d707b4d99cb
ARENA Account <pranav@arena.education>
2023-07-01 09:12:50 +0100

    save

--- dlk.py
+++ dlk.py
@@ -18,6 +18,8 @@ import elk
 
 import circuitsvis as cv
 
+from plotly_utils import imshow
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # %%
@@ -38,27 +40,22 @@ def t5_experiments():
     good
     ''',
     ]
-    l11cp = torch.load('./data/allenai/unifiedqa-t5-base/imdb/quirky-neumann/reporters/layer_11.pt')
 
     model_name = "allenai/unifiedqa-t5-base" 
     tokenizer = T5Tokenizer.from_pretrained(model_name)
     model = T5ForConditionalGeneration.from_pretrained(model_name)
 
-    def run_model(input_string, **generator_args):
-        input_ids = tokenizer.encode(input_string, return_tensors="pt")
-        res = model.generate(input_ids, **generator_args)
-        return tokenizer.batch_decode(res, skip_special_tokens=True)
     sample = imdb_ds['train']['text'][156:157]
-    output = run_model(f'''
+    input_ids = tokenizer.encode(f'''
         {sample}
-        Did the reviewer find this movie good or bad?'''
-    )
-    pp(f"{output['decoder_hidden_states'][0].shape=}")
-    pp(f"{l11cp['norm.mean_x'].shape=}")
-    #pp(samples)
-    pp((output['decoder_hidden_states'][0][0,-1] == output['decoder_hidden_states'][0][1,-1]).float().mean())
-    pp(l11cp['norm.mean_x'] @ output['decoder_hidden_states'][0][0,-1].T)
-    pp(l11cp['norm.mean_x'] @ output['decoder_hidden_states'][0][1,-1].T)
+        Did the reviewer find this movie good or bad?''', return_tensors="pt")
+    # TODO: broken forward call.
+    # output = model.forward(input_ids=input_ids, output_hidden_states=True)
+    # pp(f"{output['decoder_hidden_states'][0].shape=}")
+    # l11cp = elk.training.Reporter.load(f'./data/allenai/unifiedqa-t5-base/imdb/quirky-neumann/reporters/layer_11.pt', map_location=device)
+    # pp((output['decoder_hidden_states'][0][0,-1] == output['decoder_hidden_states'][0][1,-1]).float().mean())
+    # pp(l11cp(output['decoder_hidden_states'][0][0,-1]))
+    # pp(l11cp(output['decoder_hidden_states'][0][1,-1]))
 
 #t5_experiments()
 
@@ -70,39 +67,13 @@ gpt2_xl.eval()
 tokenizer = gpt2_xl.tokenizer
 pp(gpt2_xl)
 
-# %%
-# Or can use it from HuggingFace:
-
-# gpt2_xl : GPT2Model = GPT2Model.from_pretrained('gpt2-xl')
-# tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
-# gpt2_xl.eval()
-# pp(gpt2_xl)
-
-# %%
-sample = imdb_ds['train']['text'][156]
-sample_false = f'{sample}\nDid the reviewer find this movie good or bad?\nGood'
-sample_true = f'{sample}\nDid the reviewer find this movie good or bad?\n Bad'
-with torch.inference_mode():
-    _, cache_false = gpt2_xl.run_with_cache(sample_false, remove_batch_dim=True)
-    _, cache = gpt2_xl.run_with_cache(sample_true, remove_batch_dim=True)
-
-#%%
-for layer in range(1, 48):
-    probe = torch.load(f'./data/gpt2-xl/imdb/festive-elion/reporters/layer_{layer}.pt')
-    pp(probe.key())
-    act0 = cache_false['mlp_out', layer][-1].cpu()
-    act1 = cache['mlp_out', layer][-1].cpu()
-    p0 = (act0 @ probe['probe.0.weight'].T + probe['probe.0.bias']).sigmoid().item()
-    p1 = (act1 @ probe['probe.0.weight'].T + probe['probe.0.bias']).sigmoid().item()
-    confidence = 0.5*(p0 + (1-p1))
-    pp(f'l {layer} {p0=} {p1=} {confidence=}')
-
+#gpt2_xl : GPT2Model = GPT2Model.from_pretrained('gpt2-xl')
+#tokenizer = GPT2Tokenizer.from_pretrained('gpt2-xl')
+#gpt2_xl.eval()
+#pp(gpt2_xl)
 
 # %%
 # Experimenting with TruthfulQA dataset.
-# %%
-reporter = elk.training.Reporter.load(f'./data/gpt2-xl/imdb/festive-elion/reporters/layer_47.pt', map_location=device)
-pp(reporter)
 
 # %%
 truthfulqa = load_dataset('truthful_qa', 'generation')
@@ -121,7 +92,6 @@ pp(len(correct_statements))
 # Create dataset with x as concatenated correct and incorrect 2..4 statements,
 # and y as several 0 or 1 depending on whether a correct or incorrect statement is the correct answer.
 dataset = []
-#tokenizer = gpt2_xl.tokenizer
 while correct_statements or incorrect_statements:
     x : torch.Tensor = None
     y = []
@@ -138,63 +108,73 @@ while correct_statements or incorrect_statements:
         dataset.append((x, y))
 pp(dataset[0])        
 # %%
-# with torch.inference_mode():
-#     _, cache_true = gpt2_xl.run_with_cache(dataset[0][0])
-# pp(cache_true['mlp_out', 47].shape)
-
 with torch.inference_mode():
-    output = gpt2_xl.forward(dataset[0][0], output_hidden_states=True)
-    cache = output['hidden_states']
+    output, cache = gpt2_xl.run_with_cache(dataset[0][0])
+
+#with torch.inference_mode():
+#    output = gpt2_xl.forward(dataset[0][0], output_hidden_states=True)
+#    cache = output['hidden_states']
 # %%
-pp(f'{len(cache)=}')
-pp(f'{cache[48].shape=}')
+pp(f'{cache=}')
+
 # %%
+# Warning. Don't use torch.load.
+#reporter = elk.training.Reporter.load(f'./data/gpt2-xl/imdb/festive-elion/reporters/layer_47.pt', map_location=device)
 reporter = elk.training.Reporter.load(f'./data/gpt2-xl/dbpedia_14/reporters/layer_47.pt', map_location=device)
 reporter.eval()
 pp(reporter)
 # %%
 with torch.inference_mode():
-    #res = reporter(cache_true['mlp_out', 47][0]).sigmoid()
-    res = reporter(cache[47].to(device))[0].sigmoid()
+    res = reporter(cache['mlp_out', 47][0]).sigmoid()
+    #res = reporter(cache[47].to(device))[0].sigmoid()
 pp(res.shape)
 pp(dataset[0][1])
 for inx, label in dataset[0][1]:
     print(inx, label)
     pp(res[inx-1])
 # %%
-
 reporter = elk.training.Reporter.load(f'./data/gpt2-xl/ag_news/reporters/layer_47.pt', map_location=device)
 reporter.eval()
 pp(reporter)
 
 # %%
 with torch.inference_mode():
-    #res = reporter(cache_true['mlp_out', 47][0]).sigmoid()
-    res = reporter(cache[47].to(device))[0].sigmoid()
+    res = reporter(cache['mlp_out', 47][0]).sigmoid()
+    #res = reporter(cache[47].to(device))[0].sigmoid()
 pp(res.shape)
 pp(dataset[0][1])
 for inx, label in dataset[0][1]:
     print(inx, label)
     pp(res[inx-1])
-# %%
-reporter = torch.load(f'./data/gpt2-xl/dbpedia_14/lr_models/layer_47.pt', map_location=device)[0]
-pp(reporter)
-# %%
-with torch.inference_mode():
-    #res = reporter(cache_true['mlp_out', 47][0]).sigmoid()
-    res = reporter(cache[47].to(device))
-    res = res[0]
-pp(res.shape)
-pp(res)
-pp(dataset[0][1])
-for inx, label in dataset[0][1]:
-    print(inx, label)
-    pp(res[inx-1])
-# %%
 
+# %%
+# Visualize
 t_strs = [s.replace('Ġ', ' ') for s in tokenizer.convert_ids_to_tokens(dataset[0][0])]
-pp(t_strs)
-
 cv.tokens.colored_tokens(t_strs, res)
+
 # %%
-pp(tokenizer.decode(dataset[0][0]))
+# Inner components search (IMDB dataset)
+# %%
+sample = imdb_ds['train']['text'][156]
+sample_false = f'{sample}\nDid the reviewer find this movie good or bad?\nGood'
+sample_true = f'{sample}\nDid the reviewer find this movie good or bad?\n Bad'
+with torch.inference_mode():
+    _, cache_false = gpt2_xl.run_with_cache(sample_false, remove_batch_dim=True)
+    _, cache = gpt2_xl.run_with_cache(sample_true, remove_batch_dim=True)
+
+#%%
+layers = 48
+heads = gpt2_xl.cfg.n_heads
+head_layer_score = torch.zeros((layers, heads + 1))
+for layer in range(layers):
+    probe = elk.training.Reporter.load(
+        f'./data/gpt2-xl/imdb/festive-elion/reporters/layer_{layer}.pt', 
+        map_location=device
+    )
+    pp(probe)
+    act0 = cache_false['mlp_out', layer][-1].to(device)
+    act1 = cache['mlp_out', layer][-1].to(device)
+    p0 = probe(act0).item() # (act0 @ probe['probe.0.weight'].T + probe['probe.0.bias']).sigmoid().item()
+    p1 = probe(act1).item() # (act1 @ probe['probe.0.weight'].T + probe['probe.0.bias']).sigmoid().item()
+    confidence = 0.5*(p0 + (1-p1))
+    pp(f'l {layer} {p0=} {p1=} {confidence=}')
