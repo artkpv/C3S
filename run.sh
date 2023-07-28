56142131a0d9626dba67d13ad910e98eb59b5079
Artyom K <artyomkarpov@gmail.com>
2023-07-28 10:28:56 +0300

    save

--- run.sh
+++ run.sh
@@ -3,16 +3,19 @@ IFS=$'\n\t'
 trap "echo 'error: Script failed: see failed command above'" ERR
 
 function vast() {
-    if [[ "$VAST_CONTAINERLABEL" != "" ]] ; then
+    if [[ "${VAST_CONTAINERLABEL:-}" != "" ]] ; then
         # At Vast.
         conda env create -f environment.yml
     else
-        VAST_ID=`vast.py show instances | head -2 | tail -1 | cut -d ' ' -f`
-        if [[ "$VAST_ID" != "" ]] ; then 
-            vast.py execute $VAST_ID \
-                git clone https://github.com/artkpv/DLK-works.git  --branch dev --recurse-submodules
-            vast.py execute $VAST_ID 'cd DLK-works ; bash run.sh vast'
+        VAST_ID=`vast.py show instances | head -2 | tail -1 | cut -d ' ' -f1 `
+        if [[ "$VAST_ID" == "" ]] ; then 
+            VAST_ID=` vast.py search offers 'reliability > 0.9 disk_space > 80 gpu_ram > 16 cpu_cores >= 6 rentable=true verified=true' -o 'dph' | head -2 | tail -1 | cut -d ' ' -f1 `
+            vast.py create instance $VAST_ID --image pytorch/pytorch --disk 50
         fi
+        SSHURL=` vast.py ssh-url $VAST_ID `
+        echo running at $SSHURL
+        ssh $SSHURL git clone https://github.com/artkpv/DLK-works.git  --branch dev --recurse-submodules
+        ssh $SSHURL 'cd DLK-works ; bash run.sh vast'
     fi
 }
 
