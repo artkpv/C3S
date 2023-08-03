9a301d8ca2efc111174213783e25c3ee0a707aa5
ArtyomK <artyomkarpov@gmail.com>
2023-08-03 11:37:16 +0000

    save

--- llama.py
+++ llama.py
@@ -33,4 +33,32 @@ np_rand = np.random.default_rng(seed=42)
 # Load model
 tokenizer = LlamaTokenizer.from_pretrained("/workspace/llama/llama-2-7b-converted")
 model = LlamaForCausalLM.from_pretrained("/workspace/llama/llama-2-7b-converted")
+
 # %%
+llama = HookedTransformer.from_pretrained(
+    "/workspace/llama/llama-2-7b-converted",
+    hf_model=hf_model, 
+    device=device, 
+    fold_ln=False, 
+    center_writing_weights=False, 
+    center_unembed=False, 
+    tokenizer=tokenizer
+)
+
+# %% 
+# Try the loaded llama
+pp(llama) 
+pp(llama.generate("The capital of Germany is", max_new_tokens=20, temperature=0))
+
+# %%
+# Measure accuracies of the probes
+
+# %%
+# Load dataset
+tqa_dataset = get_tqa_dataset(np_rand)
+tqa_formated_dataset_data, tqa_formated_dataset_labels = create_tokenized_tqa_dataset(
+    llama.tokenizer, tqa_dataset, np_rand)
+
+# %%  
+
+reporter = torch.load(reporter_path, map_location=device)
