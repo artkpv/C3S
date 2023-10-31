9391a5d62793b265f4c328093ce9cdb1e1fd5081
ArtyomK <artyomkarpov@gmail.com>
2023-10-31 13:25:12 +0000

    save

--- llama.py
+++ llama.py
@@ -29,10 +29,62 @@ seed = 42
 np_rand = np.random.default_rng(seed=42)
 model_type = torch.float16
 
+from datasets import load_dataset
+from jinja2 import Environment, PackageLoader, select_autoescape
+import torch
+
+
+# %%
+def get_question_answer_dataset():
+    """
+    Returns dataset with prompts for single answer, for disjunction and for conjunction.
+    """
+    truthfulqa = load_dataset("truthful_qa", "generation")  # 817 rows
+    env = Environment(loader=PackageLoader("utils"), autoescape=select_autoescape())
+    qa_t = env.get_template("question_answer.jinja")
+    qas_t = env.get_template("question_answers.jinja")
+    qas_l_t = env.get_template("question_answers_one_line.jinja")
+    qa_dataset = []
+    qas_and_dataset = []
+    for i, row in enumerate(truthfulqa["validation"]):
+        if len(row["correct_answers"]) < 2:
+            continue
+        take_correct = i%2==0
+        for label in (True, False):
+            qa_dataset.append(
+                {
+                    "input": qa_t.render(
+                        row,
+                        is_correct_answer=take_correct,
+                        label=label,
+                    ),
+                    "label": label,
+                    "is_correct": take_correct,
+                }
+            )
+            correct_a = row["correct_answers"][0]
+            second_correct_a = row["correct_answers"][1]
+            incorrect_a = row["incorrect_answers"][0]
+            #qas_and_dataset.append(
+            #    {
+            #        "input": qas_t.render(
+            #            row,
+            #            a_A=correct_a if take_correct else incorrect_a,
+            #            a_B=second_correct_a,
+            #            is_disjunction=False,
+            #            label=label,
+            #        ),
+            #        "label": label,
+            #        "is_correct": take_correct,
+            #    }
+            #)
+    return qa_dataset, qas_and_dataset
+
+
 # %%
 
-qa_dataset, qas_dataset = get_question_answer_dataset()
-pp(qas_dataset[0])
+qa_dataset, qas_and_dataset = get_question_answer_dataset()
+#pp(qas_and_dataset[0])
 pp(qa_dataset[0])
 
 # %%
@@ -49,15 +101,39 @@ pp(model)
 
 #%%
 t_output = tokenizer(qa_dataset[0]['input'], return_tensors="pt")
-pp(t_output)
 
+# %%
+tokenized = tokenizer.tokenize(qa_dataset[0]['input'])
+pp(tokenized)
+pp(len(tokenized))
+pp(tokenizer.convert_ids_to_tokens(t_output['input_ids'][0, -1].item()))
+pp(tokenizer.encode(tokenized[-1]))
+
+#%%
+pp(qa_dataset[0])
+pp(qa_dataset[1])
 
 #%%
 outputs = model(**t_output, output_hidden_states=True)
 # %%
+
 pp(outputs.logits.shape)
 pp(tokenizer.decode(outputs.logits[0].argmax(dim=-1)))
 pp(qa_dataset[0]['input'])
+pred = outputs.logits[0, -2].softmax(dim=-1)
+pp(pred[t_output['input_ids'][0, -1].item()])
+#%%
+pp(tokenizer.convert_ids_to_tokens(pred.argmax(dim=-1).item()))
+
+# %%
+# Accuracy on the TruthfulQA dataset:
+correct = 0
+for sample in qa_dataset.values():
+    t_output = tokenizer(sample['input'], return_tensors="pt")
+    outputs = model(**t_output, output_hidden_states=False)
+    pp(tokenizer.convert_ids_to_tokens(pred.argmax(dim=-1).item()))
+
+
 
 # %%
 # Playing with LLAMA-2
@@ -89,6 +165,7 @@ pp(qa_dataset[0]['input'])
 # for seq in sequences:
 #     print(f"Result: {seq['generated_text']}")
 # 
+
 # %% 
 prompt = "Write binary search algorithm in Python. Answer:"
 batch = tokenizer(prompt, return_tensors="pt")
